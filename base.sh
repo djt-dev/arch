5e1d1662732943fcf3442b3b16cbccd124a87e45
djt-dev <ysq0508@gmail.com>
2021-10-22 17:14:23 +0800

    update

--- base.sh
+++ base.sh
@@ -18,19 +18,19 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:qwe123|chpaswd
 
 cp /etc/pacman.conf /etc/pacman.conf.back
-sed -i 's/\#ParallelDownloads = /ParallelDownloads = 5' /etc/pacman.conf
+sed -i 's/\#ParallelDownloads =/ParallelDownloads = 5/' /etc/pacman.conf
 #echo "[archlinuxcn]" >>/etc/pacman.conf
 #echo "Server = https://mirrors.aliyun.com/archlinuxcn/\$arch" >> /etc/pacman.conf
 
 pacman -S --noconfirm grub efibootmgr networkmanager bash-completion dialog
 base-devel linux-headers xdg-user-dirs
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCH
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCH
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 
 useradd -m djt
 echo djt:qwe123 | chpasswd
-useradd -aG wheel djt
-echo "djt ALL=(ALL) ALL" >> /etc/sudoers.d/djt
+usermod -aG wheel djt
+#echo "djt ALL=(ALL) ALL" >> /etc/sudoers.d/djt
