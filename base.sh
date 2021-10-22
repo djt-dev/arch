d16ce40784cfd723a4db4f199f18c270813e4106
djt-dev <ysq0508@gmail.com>
2021-10-22 17:22:45 +0800

    update

--- base.sh
+++ base.sh
@@ -15,7 +15,7 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-echo root:qwe123|chpaswd
+echo root:qwe123|chpasswd
 
 cp /etc/pacman.conf /etc/pacman.conf.back
 sed -i 's/\#ParallelDownloads =/ParallelDownloads = 5/' /etc/pacman.conf
